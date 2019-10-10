2b3e0028e17cddcc29e08ef737da7a6859ce1324
bukre <bukrenur.dincel@ozu.edu.tr>
2019-10-10 16:49:58 +0300

    changed

--- module01/src/main.java
+++ module01/src/main.java
@@ -1,7 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World! from Bükre");
+        System.out.println("Hello World! from Bukre");
+        System.out.println("Hello World! deneme");
     }
 
 }
