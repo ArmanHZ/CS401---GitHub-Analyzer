be9c55797d8b56360283010d2cfa531b272cf63a
Fatih Ar <arfatiih@gmail.com>
2019-10-26 17:27:01 +0300

    last commit is removed

--- scripts_v2/git_data_collector.sh
+++ scripts_v2/git_data_collector.sh
@@ -42,13 +42,6 @@ function formatDataDateRestriction()
 	python format_data.py $parameter
 }
 
-function nameStatusData()
-{
-	git log --name-status  > "$currentDir/nameStatusData.txt" 2>&1
-	printf "\n" >> "$currentDir/nameStatusData.txt"
-	cd "$currentDir"
-	python format_data.py
-}
 
 ###
 # Main body of script starts here
