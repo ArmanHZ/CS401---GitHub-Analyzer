4e859469a900552c11f45aa59c526afd33654b94
ArmanHZ <arman97@gmail.com>
2019-11-11 17:10:23 +0300

    Removed not used folder "module01"

--- module01/src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World! from Bukre");
-        System.out.println("Hello World! deneme");
-        System.out.println("Hello World! deneme2222");
-    }
-
-}
