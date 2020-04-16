97cdae3a6783e9df21e5befe762802039a2ea226
Arman <arman97@gmail.com>
2020-04-17 00:18:48 +0300

    Added new search bar.

--- Clustering_d3/K_means/script.js
+++ Clustering_d3/K_means/script.js
@@ -43,9 +43,12 @@ var toBeFilteredNodes = [];
 var toBeFilteredLinks = [];
 var targetNodesOfFilteredNodes = [];
 
+var labels = [];
+
 initializeNodes = () => d3.json("data.json", function (error, graph) {
     if (error) throw error;
 
+    // Test data
     nodes = [
         { "id": "Alice" },
         { "id": "Bob" },
@@ -59,8 +62,17 @@ initializeNodes = () => d3.json("data.json", function (error, graph) {
         { "source": "Alice", "target": "James" }
     ];
 
+    // Real data
     // nodes = graph.nodes;
     // links = graph.links;
+
+    function initInputAreaNames() {
+        var nodeNames = [];
+        for (let i = 0; i < nodes.length; i++)
+            nodeNames.push(nodes[i].id);
+        autocomplete(document.getElementById("nodeInput"), nodeNames);
+    }
+    initInputAreaNames();
 });
 
 initializeNodes();
@@ -130,11 +142,23 @@ function drawNodes() {
 function removeNodes() {
     d3.selectAll("circle").remove();
     d3.selectAll("line").remove();
+    resetArrays();
+    initializeNodes();
+    window.alert("Arrays are cleared!");
+
+    console.log("Labels");
+    console.log(labels);
+
+    while (labels.length != 0) {
+        labels.pop().remove();
+    }
+}
+
+function resetArrays() {
     toBeFilteredNodes.length = 0;
     toBeFilteredLinks.length = 0;
     targetNodesOfFilteredNodes.length = 0;
-    initializeNodes();
-    window.alert("Arrays cleared!");
+    // labels.length = 0;
 }
 
 function dragstarted(d) {
@@ -182,27 +206,31 @@ function mouseoutNode(d) {
 }
 
 function mouseclickNode(d) {
-    document.getElementById("nodeInputArea").value = d.id;
+    document.getElementById("nodeInput").value = d.id;
 }
 
+// TODO check for bugs
 function addNodeClicked() {
-    addNodeIfNotExists();
-    findTheLinks(); // Links of the added nodes of the previous function.
-    addTargetNodes();
-
-    // console.log(toBeFilteredNodes);
-    // console.log(toBeFilteredLinks);
+    if (addNodeIfNotExists()) {
+        findTheLinks(); // Links of the added nodes of the previous function.
+        addTargetNodes();
+    } else
+        window.alert("Node already exists!");
 }
 
 function addNodeIfNotExists() {
-    var nodeName = document.getElementById("nodeInputArea").value;
+    var nodeName = document.getElementById("nodeInput").value;
     var nodeAsJson = { "id": nodeName };
     for (let i = 0; i < nodes.length; i++) {
         if (JSON.stringify(nodes[i]) === JSON.stringify(nodeAsJson)) {
-            if (!existsInToBeFilteredNodes(nodes[i]))
+            if (!existsInToBeFilteredNodes(nodes[i])) {
                 toBeFilteredNodes.push(nodes[i]);
+                addNodeLabelToList(nodeName);
+                return true;
+            }
         }
     }
+    return false;
 }
 
 function existsInToBeFilteredNodes(node) {
@@ -211,6 +239,17 @@ function existsInToBeFilteredNodes(node) {
             return true;
     }
     return false;
+}
+
+function addNodeLabelToList(nodeName) {
+    var randomColor = randomColorGenerator();
+    var node = document.createElement("p");
+    var textnode = document.createTextNode(nodeName);
+    node.appendChild(textnode);
+    node.setAttribute("id", "labels");
+    node.style.color = randomColor;
+    document.getElementById("mySidenav").appendChild(node);
+    labels.push(node);
 }
 
 function findTheLinks() {
@@ -295,4 +334,145 @@ function applyFilterClicked() {
             .attr("cx", function (d) { return d.x; })
             .attr("cy", function (d) { return d.y; });
     }
+    applyCorrectColor();
+}
+
+function applyCorrectColor() {
+    var mainNodes = [];
+    for (let i = 0; i < labels.length; i++) {
+        var lastLabel = labels[labels.length - 1 - i];
+        d3.selectAll("circle").filter(function (node) {
+            if (node.id == lastLabel.innerHTML) {
+                mainNodes.push(node);
+                return node;
+            }
+        }).style("fill", lastLabel.style.color);
+    }
+
+    for (let i = 0; i < mainNodes.length; i++) {
+        d3.selectAll("line").filter(function (line) {
+            if (mainNodes[i].id == line.source.id) {
+                d3.selectAll("circle").filter(function (node) {
+                    if (node.id == line.target.id) {
+                        return node;
+                    }
+                }).style("fill", function (node) {
+                    for (let j = 0; j < labels.length; j++) {
+                        if (labels[j].innerHTML === mainNodes[i].id)
+                            return labels[j].style.color;
+                    }
+                });
+            }
+        });
+    }
+}
+
+function randomColorGenerator() {
+    var colorOptions = "0123456789ABCDEF";
+    var color = "#";
+    for (let i = 0; i < 6; i++) {
+        randomNum = Math.floor(Math.random() * colorOptions.length);
+        color += colorOptions.charAt(randomNum);
+    }
+    return color;
+}
+
+
+// ###################################
+// Script for autocomplete below
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function (e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].includes(val)) {
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                var substrIndex = arr[i].indexOf(val);
+                b.innerHTML = arr[i].substr(0, substrIndex);
+                b.innerHTML += "<strong>" + arr[i].substr(substrIndex, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(substrIndex + val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function (e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
 }
